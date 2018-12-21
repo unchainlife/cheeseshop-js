2c2d3a221053cfab501a98d1e8ff98da7b244981
Chris Kemp <chris.kemp3000@gmail.com>
2018-12-21 11:53:22 +0000

    Added multiple test cases

--- src/packages/cheesy-domain/src/cheese.test.js
+++ src/packages/cheesy-domain/src/cheese.test.js
@@ -25,33 +25,45 @@ describe('Cheese', () => {
   });
 
   describe('Add', () => {
-    it('should add', () => {
-      const id = uuid();
-      const name = 'Brie';
-      const description = 'Brie is a soft cow\'s-milk cheese named after Brie, the French region from which it originated. It is pale in color with a slight grayish tinge under a rind of white mould. The rind is typically eaten, with its flavor depending largely upon the ingredients used and its manufacturing environment.';
-      const date = new Date();
-      const subject = new Cheese({ id });
-      const priorState = subject.toJSON();
+    const testCases = [
+      {
+        name: 'Brie',
+        description: 'Brie is a soft cow\'s-milk cheese named after Brie, the French region from which it originated. It is pale in color with a slight grayish tinge under a rind of white mould. The rind is typically eaten, with its flavor depending largely upon the ingredients used and its manufacturing environment.',
+      }, {
+        name: 'Camembert',
+        description: 'Camembert is a moist, soft, creamy, surface-ripened cow\'s milk cheese. It was first made in the late 18th century at Camembert, Normandy, in northern France. It is similar to Brie, which is native to a different region of France.',
+      }, {
+        name: 'Cheddar',
+        description: 'Cheddar cheese is a relatively hard, off-white, sometimes sharp-tasting, natural cheese. Originating in the English village of Cheddar in Somerset, cheeses of this style are produced beyond the region and in several countries around the world.',
+      }
+    ];
+    testCases.forEach(({ name, description }) => {
+      it(`should add ${name}`, () => {
+        const id = uuid();
+        const date = new Date();
+        const subject = new Cheese({ id });
+        const priorState = subject.toJSON();
 
-      subject.add({ name, description, date });
+        subject.add({ name, description, date });
 
-      expect(subject.toJSON()).toEqual({
-        ...priorState,
-        name,
-        description,
-        valid: {
-          from: date,
-          to: null,
-        },
+        expect(subject.toJSON()).toEqual({
+          ...priorState,
+          name,
+          description,
+          valid: {
+            from: date,
+            to: null,
+          },
+        });
+
+        expect(subject.getUnpublishedEvents()).toEqual([{
+          event: CheeseAdded,
+          id,
+          name,
+          description,
+          date,
+        }]);
       });
-
-      expect(subject.getUnpublishedEvents()).toEqual([{
-        event: CheeseAdded,
-        id,
-        name,
-        description,
-        date,
-      }]);
     });
   });
 
